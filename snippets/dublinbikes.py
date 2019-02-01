310bf1ff51693ce6ad7c04a00d4461c09666d059
Arthur(Crimson) <Kingr4@protonmail.ch>
2019-02-01 16:47:55 +0000

    Testing Ingestion of Bike Data

--- snippets/dublinbikes.py
+++ snippets/dublinbikes.py
@@ -1,5 +1,6 @@
 import requests
 from pprint import pprint
+from station import Station, BikeStation
 
 API_KEY = "7a424c422928a51113b83518de8ea6d1e0cd3085"
 API_BASE = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey="
@@ -10,10 +11,20 @@ def main():
 	r = requests.get(API_BASE + API_KEY)
 	print(r.status_code)
 
-	for station in r.json():
-		pprint(station['name'])
-		pprint(station['available_bikes'])
-		pprint(r.json())
+	list = []
+	for i in r.json():
+		tmpStation = BikeStation(i['name'],
+		i['position']['lat'], i['position']['lng'],
+		i['address'],
+		i['status'],
+		i['last_update'],
+		i['available_bikes'], i['bike_stands'],
+		i['available_bike_stands']
+		)
 
+		list.append(tmpStation)
+
+	for station in list:
+		pprint(station)
 
 main()
