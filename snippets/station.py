583f3c209be6e3d7063eb22c904b1a84403ffb3c
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-02-01 16:47:55 +0000

    Testing Ingestion of Bike Data

--- snippets/station.py
+++ snippets/station.py
@@ -1,21 +1,24 @@
 class Station:
 
-	def __init__(self, name, lat, long):
+	def __init__(self, name, lat, lng):
 		self.name = name
 		self.lat = lat
-		self.long = long
+		self.lng = lng
 
 	def getLocation(self):
-		return self.lat + self.long
+		return self.lat + self.lng
 
 
 class BikeStation(Station):
 
-	def __init__(self,name,lat,long, address, status, lastUpdate, numBikes, numStands, availableStands):
-		Station.__init__(self,name,lat,long)
+	def __init__(self,name,lat,lng, address, status, lastUpdate, numBikes, numStands, availableStands):
+		Station.__init__(self,name,lat,lng)
 		self.address = address
 		self.status = status
 		self.lastUpdate = lastUpdate
 		self.numBikes = numBikes
 		self.numStands = numStands
 		self.availableStands = availableStands
+
+	def __str__(self):
+		return str(self)
